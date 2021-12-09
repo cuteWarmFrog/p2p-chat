ebd2d63fab103f6c87881ebee9edbd16426bb0e3
cuteWarmFrog <livshits.victor@gmail.com>
2021-12-09 14:48:15 +0300

    added p2p-server

---
+++ p2p-server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
+const { ExpressPeerServer } = require('peer');
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketio(server).sockets;
+
+app.use(express.json());
+
+const idGenerator = () => {
+    return (Math.random().toString(36) + "0000000000000000000").substr(2, 16);
+}
+
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+    path: '/',
+    generateClientId: idGenerator
+})
+
+app.use('/mypeer', peerServer);
+
+io.on('connection', (socket) => {
+    socket.on('join-room', ({ roomId, userId }) => {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit('user-connected', userId);
+    })
+})
+
+const port = 6000;
+
+server.listen(port, () => console.log(`Server is running on ${port} port}`));
