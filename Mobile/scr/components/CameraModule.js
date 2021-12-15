a2f02cbc4fef62ea820380ace2ec3323a8b4d891
alex-grandson <285484@niuitmo.ru>
2021-12-15 04:50:37 +0300

    refactor styles CameraModule.js

--- Mobile/scr/components/CameraModule.js
+++ Mobile/scr/components/CameraModule.js
@@ -23,27 +23,10 @@ const styles = StyleSheet.create({
     camera: {
         // flex: Platform.OS === 'ios' ? 1 : 0,
         // height: Platform.OS === 'ios' ? 'auto' : '100%',
-        height: "100%",
-        width: '100%',
-        padding: 10,
-        flexDirection: 'row-reverse',
-        justifyContent: 'flex-end',
-        alignItems: 'flex-end'
-    },
-    buttonContainer: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        width: '120%',
+        marginLeft: "-10%",
 
-        backgroundColor: 'rgb(40,60,50)',
-        borderColor: 'rgb(0,0,0)',
-        borderWidth: 3,
-
-        width: 40,
-        height: 40,
-        borderRadius: 12,
-        position: 'absolute',
-        bottom: 10,
-        right: 10
     },
+
 });
