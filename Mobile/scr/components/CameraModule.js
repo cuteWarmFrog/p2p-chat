bac815c2e798705074187e576a9c18f1a33db8bf
alex-grandson <285484@niuitmo.ru>
2021-12-18 19:19:53 +0300

    fix 2 participants render

--- Mobile/scr/components/CameraModule.js
+++ Mobile/scr/components/CameraModule.js
@@ -1,39 +1,35 @@
-import React, { useState, useCallback } from 'react';
+import React, { useCallback } from 'react';
 import {
     StyleSheet,
-    View,
-    Image
+    View as DefaultView
 } from 'react-native';
-
-import doggo from '../constants/doggo.jpg';
-const DEFAULT_IMAGE = Image.resolveAssetSource(doggo).uri;
+import {
+    View,
+    Text
+} from './Themed';
 
 import {RTCView} from "react-native-webrtc";
 
 export default function CameraModule(props) {
-    const [hasPermission, setHasPermission] = useState(null);
-    // const [type, setType] = useState(Camera.Constants.Type.front);
-    const { stream } = props;
-
-    console.log(stream.getVideoTracks);
+    const { stream, zOrder } = props;
 
     const renderStream = useCallback(() => {
-        console.log('streamEnable:', stream.getVideoTracks()[0].enabled);
         if(stream.getVideoTracks()[0].enabled) {
-            return <RTCView style={styles.camera} streamURL={stream.toURL()} />
+            return <RTCView zOrder={zOrder ? zOrder : 0} style={styles.camera} streamURL={stream.toURL()} />
         }
         return (
-            <Image
-                style={styles.img}
-                source={{uri: DEFAULT_IMAGE}}
-            />
+            <View style={styles.noImage}>
+                <Text>
+                    No image
+                </Text>
+            </View>
         )
     }, [stream]);
 
     return (
-        <View style={styles.camera}>
+        <DefaultView style={styles.camera}>
             {renderStream()}
-        </View>
+        </DefaultView>
     );
 }
 
@@ -46,11 +42,9 @@ const styles = StyleSheet.create({
         marginLeft: "-10%",
 
     },
-
-    img: {
-        flex: 1,
-        width: '100%',
-        height: '100%',
-        resizeMode: 'cover',
+    noImage: {
+        height: "100%",
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 });
