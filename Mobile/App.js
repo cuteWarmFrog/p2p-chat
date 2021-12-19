4ca6feee1709f90b70db27b4bb40d31df3c1dcf5
alex-grandson <285484@niuitmo.ru>
2021-12-19 04:24:50 +0300

    hide stack title

--- Mobile/App.js
+++ Mobile/App.js
@@ -12,7 +12,11 @@ const Stack = createStackNavigator();
 export default function App(){
     return(
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false
+                }}
+            >
                 <Stack.Screen name="Home" component={Home}/>
                 <Stack.Screen name="Chat" component={Chat}/>
             </Stack.Navigator>
